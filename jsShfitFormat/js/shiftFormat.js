53d4d0c7b60a81111b1cc65e34d75462477c5fac
Kim Daewook <lostfind@users.noreply.github.com>
2016-11-30 15:38:58 +0900

    for init Load Test

--- jsShfitFormat/js/shiftFormat.js
+++ jsShfitFormat/js/shiftFormat.js
@@ -1,274 +1,78 @@
-const DAY_SHIFT   = ["", "F"];
-const TWO_SHIFT   = ["", "D", "E"];
-const THREE_SHIFT = ["", "A", "B", "C"];
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Shift Format</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="./css/shiftTable.css">
+</head>
 
-const MAX_TEAM = 26;
-const MAX_DAYS = 30;
-const HEAD_ROWS = 2;
+<body>
 
-var selectedPattern = "day";
-var shiftPattern = document.getElementById("shiftPattern");
+    <div class="wrapperArea">
+        <div class="selectArea">
+            <lable>Shift Pattern : </lable>
+            <select id="shiftPattern">
+                <option value="day">Day Shift</option>
+                <option value="two">2 shift</option>
+                <option value="three">3 Shift</option>
+            </select>
+        </div>
 
-// 교대 패턴 변경 시
-shiftPattern.onchange = function () {
-    selectedPattern = this.value;
-    clearShift();
-};
+        <div class="tableArea clearfix">
+            <!-- colButton START -->
+            <div class="colButtonArea">
+                <ul id="colButton">
+                    <li><a href="#" onclick="addCol(this)">+</a> <a href="#" onclick="removeCol(this)">-</a></li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+            <!-- colButton END -->
 
-// 교대 타입 칸 비움
-function clearShift() {
-    var shiftTypes = document.getElementsByClassName("shiftType");
+            <!-- rowButton START -->
+            <div class="rowButtonArea">
+                <ul id="rowButton">
+                    <li><a href="#" onclick="addRow(this)">+</a> <a href="#" onclick="removeRow(this)">-</a></li>
+                </ul>
+            </div>
+            <!-- rowButton END -->
 
-    for ( var i = 0; i < shiftTypes.length; i++ ) {
-        shiftTypes[i].textContent = "";
-    }
-}
+            <!-- shfit Table START -->
+            <div class="shiftTableArea">
+                <table id="shiftTable">
+                    <thead>
+                        <tr>
+                            <td class="teamHeader" rowspan="2">Team Name</td>
+                            <td id="daysHeader" colspan="5">Number of Days</td>
+                        </tr>
+                        <tr>
+                            <td class="shiftDay">1</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="teamName">Team A</td>
+                            <td class="shiftType" id="1-1" onclick="clickShift(id)"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- shfit Table END -->
+        </div>
 
-// 교대 타입 칸 클릭시
-function clickShift(id) {
-    var selectedCell = document.getElementById(id);
-    var num;
+        <!-- func button START -->
+        <div class="buttonArea clearfix">
+            <input class="btn" id="save" type="button" value="Save" onclick="saveShift()"></input>
+            <input class="btn" id="init" type="button" value="Init" onclick="init()"></input>
+            <input class="btn" id="load" type="button" value="Load" onclick="loadShift()"></input>
+        </div>
+        <!-- func button END -->
 
-    if ( selectedPattern === "day" ) {
-        num = DAY_SHIFT.indexOf(selectedCell.textContent);
-        num = (++num % 2);
-        selectedCell.textContent = DAY_SHIFT[num];
-    } else if ( selectedPattern === "two" ) {
-        num = TWO_SHIFT.indexOf(selectedCell.textContent);
-        num = (++num % 3);
-        selectedCell.textContent = TWO_SHIFT[num];
-    } else if ( selectedPattern === "three" ) {
-        num = THREE_SHIFT.indexOf(selectedCell.textContent);
-        num = (++num % 4);
-        selectedCell.textContent = THREE_SHIFT[num];
-    }
-}
+    </div>
 
+</body>
 
-/* *************************
- *  Table add/Remove (row, col)
- ************************* */
-// 열 추가
-function addCol(item) {
-    var $colButton = $(item.parentElement);
-    var shiftTable = document.getElementById("shiftTable");
-    var colLength  = shiftTable.rows[HEAD_ROWS].cells.length;
-    var rowLength  = shiftTable.rows.length;
-    var daysHeader = document.getElementById("daysHeader");
-    var colId      = ( maxIdVal("col") + 1 );
-    var objCell;
-    var rowId;
+<script src="./js/vendor/jquery-3.1.1.js"></script>
+<script src="./js/testData.js"></script>
+<script src="./js/shiftFormat.js"></script>
 
-    if ( colLength <= MAX_DAYS ) {
-        objCell = shiftTable.tHead.rows[1].insertCell( $colButton.index() );
-        objCell.className = "shiftDay";
-
-        for ( var i = HEAD_ROWS; i < rowLength; i++ ) {
-            objCell = shiftTable.rows[i].insertCell( $colButton.index() + 1 );
-            rowId = shiftTable.rows[i].lastElementChild.id.split("-")[0];
-            objCell.className = "shiftType";
-            objCell.id = rowId + "-" + colId;
-            objCell.setAttribute("onclick", "clickShift(id)");
-            objCell.innerHTML = "";
-        }
-
-        // daysHeader colspan set
-        daysHeader.setAttribute("colspan", shiftTable.rows[1].cells.length);
-
-        addPlusMinus("col");
-        namingDays();
-
-    }
-}
-
-// 열 삭제 버튼
-function removeCol(item) {
-    var $colButton = $(item.parentElement),
-        shiftTable = document.getElementById("shiftTable"),
-        rowLength  = shiftTable.rows.length;
-
-    if ( shiftTable.tHead.rows[1].cells.length > 1 ) {
-        shiftTable.tHead.rows[1].deleteCell( $colButton.index() );
-
-        for ( var i = HEAD_ROWS; i < rowLength; i++ ) {
-            shiftTable.rows[i].deleteCell( $colButton.index() + 1 );
-        }
-    }
-
-    removePlusMinus("col");
-    namingDays();
-}
-
-
-// 행 추가
-function addRow(item) {
-    var $rowButton = $(item.parentElement);
-    var shiftTable = document.getElementById("shiftTable");
-    var colLength  = shiftTable.rows[HEAD_ROWS].cells.length;
-    var rowLength  = shiftTable.rows.length;
-    var objRow     = shiftTable.insertRow( $rowButton.index() + 2 );
-    var rowId      = maxIdVal("row") + 1;
-    var objCell;
-
-    if ( rowLength < MAX_TEAM + HEAD_ROWS ) {
-        objCell = objRow.insertCell(0);
-        objCell.className = "teamName";
-
-        for ( var i = 1; i < colLength; i++ ) {
-            objCell = objRow.insertCell(i);
-            objCell.className = "shiftType";
-            objCell.id = rowId + "-" + i;
-            objCell.setAttribute("onclick", "clickShift(id)");
-            objCell.innerHTML = "";
-        }
-
-        addPlusMinus("row");
-        namingTeam();
-    }
-}
-
-// 행 삭제
-function removeRow(item) {
-    var $rowButton = $(item.parentElement);
-    var shiftTable = document.getElementById("shiftTable");
-
-    if ( shiftTable.rows.length > HEAD_ROWS + 1 ) {
-        shiftTable.deleteRow( $rowButton.index() + HEAD_ROWS );
-    }
-
-    removePlusMinus("row");
-    namingTeam();
-}
-
-
-/* *************************
- *  Table Header Rename (row, col)
- ************************* */
-// Shift Team 명칭 부여
-function namingTeam() {
-    var teamList = document.getElementsByClassName("teamName");
-    var len      = teamList.length;
-
-    for ( var i = 0; i < len; i++ ) {
-        teamList[i].innerText = "Team " + String.fromCharCode( 65 + i );
-    }
-}
-
-// 일자 헤더 재정렬
-function namingDays() {
-    var dayList = document.getElementsByClassName("shiftDay"),
-        len     = dayList.length;
-
-    for ( var i = 0; i < len; i++ ) {
-        dayList[i].innerText = ( i + 1 );
-    }
-}
-
-
-/* *************************
- *  ( + - ) Button Function
- ************************* */
-// + - 버튼 추가
-function addPlusMinus(division) {
-    var buttonElem,
-        itm,
-        cln;
-
-    if ( division === "row" ) {
-        buttonElem = document.getElementById("rowButton");
-        itm = buttonElem.firstElementChild;
-        cln = itm.cloneNode(true);
-
-        buttonElem.appendChild(cln);
-
-    } else if ( division === "col" ) {
-        buttonElem = document.getElementById("colButton");
-        itm = buttonElem.firstElementChild;
-        cln = itm.cloneNode(true);
-
-        buttonElem.appendChild(cln);
-    }
-}
-
-// + - 버튼 삭제
-function removePlusMinus(division) {
-    var buttonElem;
-
-    if ( division === "row" ) {
-        buttonElem = document.getElementById("rowButton");
-    } else if ( division === "col" ) {
-        buttonElem = document.getElementById("colButton");
-    }
-
-    if ( buttonElem !== undefined && buttonElem.children.length > 1) {
-        buttonElem.removeChild(buttonElem.lastElementChild);
-    }
-}
-
-
-// ID 최대치 반환
-function maxIdVal( division ) {
-    var shiftTypes = document.getElementsByClassName("shiftType");
-    var len        = shiftTypes.length;
-    var shiftTypeId,
-        idArray = [];
-
-    if ( division === "row" ) {
-        for ( var i = 0; i < len; i++ ) {
-            shiftTypeId = shiftTypes[i].id;
-            idArray.push(shiftTypeId.split("-")[0]);
-        }
-        return Math.max.apply(null, idArray);
-
-    } else if ( division === "col" ) {
-        for ( var i = 0; i < len; i++ ) {
-            shiftTypeId = shiftTypes[i].id;
-            idArray.push(shiftTypeId.split("-")[1]);
-        }
-        return Math.max.apply(null, idArray);
-    }
-}
-
-
-// 저장 버튼 클릭
-function save() {
-    var shiftTeamArray = [],
-        shiftDayArray  = [];
-    var teamList  = document.getElementsByClassName("teamName"),
-        dayList   = document.getElementsByClassName("shiftDay"),
-        shiftList = document.getElementsByClassName("shiftType");
-
-    var shiftListLen = shiftList.length,
-        teamListLen  = teamList.length,
-        dayListLen   = dayList.length;
-
-    var shiftObj = {},
-        shiftArr = [],
-        shiftListJSON;
-
-    for ( var i = 0; i < teamListLen; i++ ) {
-        shiftTeamArray.push(teamList[i].innerText);
-    }
-
-    for ( var i = 0; i < dayListLen; i++ ) {
-        shiftDayArray.push(dayList[i].innerText);
-    }
-
-    for ( var i = 0; i < shiftListLen; i++ ) {
-
-        if ( shiftList[i].innerText !== "" ) {
-            shiftObj = {};
-            shiftObj.team = shiftTeamArray[Math.floor(i / dayListLen)];
-            shiftObj.days = shiftDayArray[(i % dayListLen)];
-            shiftObj.shift = shiftList[i].innerText;
-
-            shiftArr.push(shiftObj);
-        }
-    }
-
-    shiftListJSON = JSON.stringify(shiftArr);
-
-    // TEST
-    alert(shiftListJSON);
-    console.log(shiftListJSON);
-}
+</html>
