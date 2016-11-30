e8a1c8f97e9f4c97023d63232fff277eeab59ac2
Kim Daewook <lostfind@users.noreply.github.com>
2016-11-30 15:37:58 +0900

    for init Load Test

---
+++ jsShfitFormat/js/testData.js
@@ -0,0 +1,4 @@
+var initFlg = false;
+var countDay = 14;
+var countTeam = 3;
+var shiftInfo = '{"data":[{"team":"Team A","days":"7","shift":"F"},{"team":"Team B","days":"3","shift":"F"},{"team":"Team B","days":"13","shift":"F"},{"team":"Team C","days":"11","shift":"F"}]}';
